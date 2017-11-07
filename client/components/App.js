9b02dc819e68c5d547daa975dd01adf7a6cc390c
Xin Liu <xin.liu@move.com>
2017-11-06 23:13:42 -0800

    init

---
+++ client/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+  render(){
+    return(
+      <div className="my-css-class">
+        <h1 className="h1">My first page</h1>
+      </div>
+    );
+  }
+}
