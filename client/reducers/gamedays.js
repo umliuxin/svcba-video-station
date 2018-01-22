d34a30122fde940203a31a3eb53da5656ddf2fa1
Xin Liu <xinliu.gl@gmail.com>
2018-01-21 20:18:56 -0800

    cc fix

--- client/reducers/gamedays.js
+++ client/reducers/gamedays.js
@@ -2,13 +2,11 @@ import { FETCH_VIDEO_ACTION } from '../constants/actions';
 
 
 export default function(thisState = null, action){
-  if (action.type === FETCH_VIDEO_ACTION){
-    if (thisState == null){
-      if (action.payload.data){
-        return action.payload.data.gamedays;
-      } else {
-        return null;
-      }
+  if (action.type === FETCH_VIDEO_ACTION && thisState == null){
+    if (action.payload.data){
+      return action.payload.data.gamedays;
+    } else {
+      return null;
     }
   }
   return thisState;
