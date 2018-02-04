66ab2fe744e5edc874737f665673751f70c95064
Xin Liu <xinliu.gl@gmail.com>
2018-02-03 22:02:49 -0800

    team list

--- client/constants/constants.js
+++ client/constants/constants.js
@@ -1,2 +1,6 @@
 export const API_URL = 'http://www.lsjball.club/api/videos?app=video-station';
 export const YOUTUBE_BASE_URL = "https://www.youtube.com/embed/";
+
+
+export const GROUP_MEN = "men";
+export const GROUP_WOMEN = "women";
--- client/containers/team-list.js
+++ client/containers/team-list.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { selectTeam, fetchVideos } from '../actions';
 import { bindActionCreators } from 'redux';
 
+import { GROUP_MEN, GROUP_WOMEN } from '../constants/constants';
+
 class TeamList extends Component {
   constructor(props){
     super(props);
@@ -11,37 +13,74 @@ class TeamList extends Component {
   onItemClick(team = null){
     this.props.selectTeam(team);
   }
-  renderList(){
-    return Object.keys(this.props.teams).map((key) => {
+  renderListItem(team, isActive){
+    if (isActive){
+      return (
+        <div
+          className="c_team-list-item col-sm-2 active"
+          key={team.id}
+          onClick={() => {
+            this.props.selectTeam();
+            this.props.fetchVideos();
+          }}>
+          <div className="wrap">
+            <img src={team.image}/>
+            <div className="text-wrap"><div>{team.name}</div></div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div
+          className="c_team-list-item col-sm-2"
+          key={team.id}
+          onClick={() => {
+            this.props.selectTeam(team);
+            this.props.fetchVideos();
+          }}>
+          <div className="wrap">
+            <img src={team.image}/>
+            <div className="text-wrap"><div>{team.name}</div></div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  renderList(group = 'all'){
+    return Object.keys(this.props.teams)
+      .filter(key => {
+        if (group == "all") return true;
+        return this.props.teams[key].group === group;
+      })
+      .map(key => {
       let team = this.props.teams[key];
-      if (this.props.selectedTeam && team.id === this.props.selectedTeam.id){
-        return (
-          <div
-            className="team-list-item btn btn-primary active"
-            key={team.id}
-            onClick={() => {
-              this.props.selectTeam();
-              this.props.fetchVideos();
-            }}>Active: {team.name}</div>
-        );
-      } else {
-        return (
-          <div
-            className="team-list-item btn btn-primary"
-            key={team.id}
-            onClick={() => {
-              this.props.selectTeam(team);
-              this.props.fetchVideos();
-            }}>{team.name}</div>
-        );
-      }
+      return this.renderListItem(team, this.props.selectedTeam && team.id === this.props.selectedTeam.id);
     });
+  }
+  renderSelected(){
+    if (!this.props.selectedTeam) return '';
+    return (
+      <h4>SELECTED</h4>
+    );
   }
 
   render(){
     return (
-      <div className='team-list'>
-        {this.renderList()}
+      <div className='c_team-list'>
+        <div className='container'>
+          <h2 className="comp-title">Filter by team</h2>
+          {this.renderSelected()}
+          <h4> MEN Group</h4>
+          <div className="row">
+            {this.renderList(GROUP_MEN)}
+          </div>
+          <h4>Women Group</h4>
+          <div className="row">
+            {this.renderList(GROUP_WOMEN)}
+          </div>
+
+        </div>
       </div>
     );
   }
