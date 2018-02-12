962a946927845414914f44b2a67de4046fd3543e
Xin Liu <xinliu.gl@gmail.com>
2018-02-11 18:07:09 -0800

    add tracking

--- client/constants/constants.js
+++ client/constants/constants.js
@@ -1,5 +1,5 @@
-// export const API_URL = 'http://www.lsjball.club/api/videos?app=video-station';
-export const API_URL = 'http://localhost:3000/api/videos?app=video-station';
+export const API_URL = 'http://www.lsjball.club/api/videos?app=video-station';
+// export const API_URL = 'http://localhost:3000/api/videos?app=video-station';
 export const YOUTUBE_BASE_URL = "https://www.youtube.com/embed/";
 
 
